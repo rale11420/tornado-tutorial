8a325e32ecd7d6bf9337cf82c48393b45dae415a
Rastko Misulic <rastko23@hotmail.com>
2024-05-13 12:43:00 +0200

    Add networks and api keys

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Project from tutorial:
 
 Tornado Cash: https://www.youtube.com/playlist?list=PL_SqG412uYVYtEM8B8xNccFyhrGsMV1MG
+
+
+# TO DO
+add styling <br>
+add natspec
+...
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,28 @@
+const { version } = require("hardhat");
+
 require("@nomicfoundation/hardhat-toolbox");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24"
+      }
+    ]
+  },
+  networks: {
+    goerli: {
+      url: process.env.NODE_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.API_KEY
+    }
+  }
 };
